250eeed61bf5df85e1b0da943c2f5ec18f9dd528
Aaditya Nair <128372678+FreshPrince99@users.noreply.github.com>
2024-01-26 17:19:07 +0000

    Pending changes exported from your codespace

--- app.py
+++ app.py
@@ -1,15 +1,28 @@
 import streamlit as st
 import requests
 from streamlit_lottie import st_lottie
-
+import torchvision
+import numpy as np
 from pathlib import Path
-from test import *
-from code import *
+
+import torch
+from torch import nn
+import pandas as pd
+from sklearn.metrics import mean_absolute_error
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeRegressor
+from torchvision.datasets import ImageFolder
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+
+import torch
+from torch import nn
+# from model_LogR import *
 with st.container():
     st.title(f" Deep Learning Project website ")
     st.subheader("Cancerous CT scan detection using computer vision 🤖")
     st.write("Please enter image below 👇")
-    st.write(model_results)
+    # st.write(model_results)
 
 def load_lottieurl(url):
     r= requests.get(url)
@@ -29,6 +42,23 @@ with st.container():
         uploaded_file = st.file_uploader(label = 'Insert image', accept_multiple_files = False,
                                         type = ['png','jpeg','jpg'])
         # Now this image will be sent to the main model file to predict and bring back the prediction as well as loss and accuracy values
+        uploaded_file = uploaded_file.read()
+        uploaded_file=torch.Tensor(np.frombuffer(uploaded_file, dtype=np.int32))
+        convert = transforms.ToTensor()
+        converted_file = uploaded_file.type(torch.float32) 
+        # uploaded_file = torchvision.io.decode_jpeg(uploaded_file)
+        st.write(converted_file)
+        st.write(converted_file.dtype)
+        st.write(converted_file.shape)
+
+        custom_image_transform = transforms.Compose([
+            transforms.Resize(size=(64, 64))
+        ])
+
+        # Transform target image
+        custom_image_transformed = custom_image_transform(converted_file)
+        st.write(custom_image_transformed.shape)
+
         
     # with left_column:
     #     st.header("A Brief summary ")
